c5fa31ae796d2ee7a8ae381b4a0bdf39b69cdc2b
Edwin Carter <edwincarter39350@gmail.com>
2024-04-03 19:24:23 -0400

    Finally working version, added the output to serial. Data comes in from network port 8000 and goes out serial port COM5 by default.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
-#include <string>
-#include "serial/serial.h"
 using namespace std;
 
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "8000" // Change this to the port you want to listen on
-//#define DEFAULT_SERIAL_PORT ""
+const int    DEFAULT_BUFLEN =       512;
+const char*  DEFAULT_NET_PORT =     "8000"; 
+
+char lastErr[1020];
 
 int main(int argc, char** argv) {
     WSADATA wsaData;
@@ -18,10 +17,9 @@ int main(int argc, char** argv) {
     struct addrinfo* result = NULL,
         * ptr = NULL,
         hints;
-    const char* sendbuf = "this is a test";
-    char recvbuf[DEFAULT_BUFLEN];
     int iResult;
-    int recvbuflen = DEFAULT_BUFLEN;
+
+
 
     // Validate the parameters
     if (argc != 2) {
@@ -42,7 +40,7 @@ int main(int argc, char** argv) {
     hints.ai_protocol = IPPROTO_TCP;
 
     // Resolve the server address and port
-    iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+    iResult = getaddrinfo(argv[1], DEFAULT_NET_PORT, &hints, &result);
     if (iResult != 0) {
         printf("getaddrinfo failed with error: %d\n", iResult);
         WSACleanup();
@@ -78,26 +76,89 @@ int main(int argc, char** argv) {
         WSACleanup();
         return 1;
     }
-
-    //Create serial port on default serial port
-
-    // Receive until the peer closes the connection
-    bool isNum = 0;
-    uint8_t num = 
-    do {
-
-        iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
-        for (int i = 0; i < iResult; i++) {
-            if (recvbuf[i] >= 48 && recvbuf[i] <= 57) {
-                if (isNum = 0) {
-
-                }
-                num = 1;
-            }
-            else{
-                printf("%c", recvbuf[i]);
-            }
+    //Open serial port
+    
+    HANDLE h_Serial;
+    h_Serial = CreateFileA("COM5", GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL, 0);
+    if (h_Serial == INVALID_HANDLE_VALUE) {
+        if (GetLastError() == ERROR_FILE_NOT_FOUND) {
+            // serial port not found. Handle error here.
+            FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL,
+                GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                lastErr, 1020, NULL);
+            printf("%s", lastErr);
+            return 1;
         }
+        // any other error. Handle error here.
+        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL,
+            GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            lastErr, 1020, NULL);
+        printf("%s", lastErr);
+        cerr << "Unknown Exception" << endl;
+        return 1;
+    }
+
+    DCB dcbSerialParam = { 0 };
+    dcbSerialParam.DCBlength = sizeof(dcbSerialParam);
+
+    if (!GetCommState(h_Serial, &dcbSerialParam)) {
+        // handle error here
+    }
+
+    dcbSerialParam.BaudRate = CBR_9600;
+    dcbSerialParam.ByteSize = 8;
+    dcbSerialParam.StopBits = ONESTOPBIT;
+    dcbSerialParam.Parity = NOPARITY;
+
+    if (!SetCommState(h_Serial, &dcbSerialParam)) {
+        // handle error here
+    }
+
+    COMMTIMEOUTS timeout = { 0 };
+    timeout.ReadIntervalTimeout = 60;
+    timeout.ReadTotalTimeoutConstant = 60;
+    timeout.ReadTotalTimeoutMultiplier = 15;
+    timeout.WriteTotalTimeoutConstant = 60;
+    timeout.WriteTotalTimeoutMultiplier = 8;
+    if (!SetCommTimeouts(h_Serial, &timeout)) {
+        // handle error here
+    }
+
+
+
+
+    char* writeBuff = new char[DEFAULT_BUFLEN];
+    char* readBuff = new char[DEFAULT_BUFLEN];
+    DWORD numWritten = 0;
+    DWORD numRead = 0;
+    // Receive until the peer closes the connection
+    do {
+        iResult = recv(ConnectSocket, writeBuff, DEFAULT_BUFLEN, 0);
+        /*for (int i = 0; i < iResult; i++) {
+            printf("%c", writeBuff[i]);
+        }
+        printf("\n");*/
+
+        
+        if (!WriteFile(h_Serial, writeBuff, iResult, &numWritten, NULL)) {
+            FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL,
+                GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                lastErr, 1020, NULL);
+            printf("%s", lastErr);
+        }
+        if (!ReadFile(h_Serial, readBuff, numWritten, &numRead, NULL)) {
+            FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL,
+                GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                lastErr, 1020, NULL);
+            printf("%s", lastErr);
+        }
+
+        for (int i = 0; i < numRead; i++) {
+            printf("%c", readBuff[i]);
+        }
+
+        printf("\n");
         if (iResult > 0) {
             //printf("Bytes received: %d\n", iResult);
         }
@@ -111,7 +172,10 @@ int main(int argc, char** argv) {
     } while (iResult > 0);
 
     
+    
+    
     // cleanup
+    CloseHandle(h_Serial);
     closesocket(ConnectSocket);
     WSACleanup();
 
